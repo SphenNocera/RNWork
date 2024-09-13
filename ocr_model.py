02deca86d476a523f957b6060526c35bdc4f26e3
SphenNocera <noceraa432@gmail.com>
2024-09-12 20:35:20 -0400

    Comparing averages

--- ocr_model.py
+++ ocr_model.py
@@ -2,11 +2,17 @@ import pytesseract
 from PIL import Image
 from pdf2image import convert_from_path
 import re
-import cv2
+import cv2 as cv
+import os
+import numpy as np
 
-path_to_file = "docs/document-1.pdf"
+path_to_file = os.path.abspath(os.path.join(os.getcwd(), "docs/document-1.pdf"))
 
 def convert_pdf_to_image(file):
+    """
+    Converts a pdf file into a PIL image type
+    """
+
     images = convert_from_path(path_to_file)
     return images
 
@@ -25,18 +31,43 @@ def get_average_confidence_level(word_conf_dict: dict):
     total = sum(word_conf_dict.values())
     return total / len(word_conf_dict)
 
+def get_image_data_for_modification(file):
+    images = convert_pdf_to_image(file)
 
-images = convert_pdf_to_image(path_to_file)
-first_page_image = images[1]
+    # must convert to numpy array for opencv preprocessing
+    converted_images = [cv.cvtColor(np.array(image), cv.COLOR_BGR2RGB) for image in images]
 
-first_page_image.show()
+    img = converted_images[0]
 
-page_data = pytesseract.image_to_data(first_page_image, output_type=pytesseract.Output.DICT)
+    return img
 
-# print(page_data["text"])
-confidence_per_word = get_confidence_level_per_word(page_data)
-print(confidence_per_word)
-avg_confidence = get_average_confidence_level(confidence_per_word)
+    # cv.imshow("Original image", img)
+    # cv.waitKey()
 
-print(avg_confidence)
+def compare_averages(word_conf1, word_conf2):
+    avg1 = get_average_confidence_level(word_conf1)
+    avg2 = get_average_confidence_level(word_conf2)
+
+    print("base: " + str(avg1) + "\nInverted: " + str(avg2))
+
+def invert_image(image):
+    return cv.bitwise_not(image)
+    
+
+inverted_img = pytesseract.image_to_data(invert_image(get_image_data_for_modification(path_to_file)), output_type=pytesseract.Output.DICT)
+base_image = pytesseract.image_to_data(convert_pdf_to_image(path_to_file)[0], output_type=pytesseract.Output.DICT)
+
+compare_averages(get_confidence_level_per_word(base_image), get_confidence_level_per_word(inverted_img))
+
+
+
+
+# page_data = pytesseract.image_to_data(first_page_image, output_type=pytesseract.Output.DICT)
+
+# # print(page_data["text"])
+# confidence_per_word = get_confidence_level_per_word(page_data)
+# print(confidence_per_word)
+# avg_confidence = get_average_confidence_level(confidence_per_word)
+
+# print(avg_confidence)
 
